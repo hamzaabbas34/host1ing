904e7a8b17a986cba0e2ac776d76a1cd1bd8f2a4
hamzaabbas34 <abbasbahi289@gmail.com>
2024-10-16 09:31:48 +0500

    as

--- src/component/Home/Home.jsx
+++ src/component/Home/Home.jsx
@@ -1,5 +1,5 @@
 import React from "react";
 
 export default function Home() {
-	return <div>Home</div>;
+	return <div> Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos nemo distinctio sint perferendis quis fugiat non officiis, adipisci nesciunt a asperiores, dolorem quod vitae. Debitis consectetur reprehenderit unde aut laborum? Nostrum praesentium sed commodi architecto adipisci quasi alias dolore dolorum earum, natus perspiciatis quae vero, delectus, nemo quam hic tempora!</div>;
 }
